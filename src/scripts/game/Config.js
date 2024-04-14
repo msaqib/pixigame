b982de85d4db0d96a922c53e184a0bef7194cd9b
Muhammad Saqib Ilyas <msaqib@gmail.com>
2024-04-14 18:26:05 +0500

    Consolidated mp3 and png loading code

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -3,8 +3,7 @@ import { GameScene } from "./GameScene";
 import { Game } from "./Game";
 
 export const Config = {
-    loader: Tools.importAll(require.context('./../../sprites', true, /\.(png)$/)),
-    soundLoader: Tools.importAll(require.context('./../../sprites', true, /\.(mp3)$/)),
+    loader: Tools.importAll(require.context('./../../sprites', true, /\.(png|mp3)$/)),
     bgSpeed: 2,
     score: {
         x: 10,
--- src/scripts/system/Loader.js
+++ src/scripts/system/Loader.js
@@ -5,34 +5,33 @@ export class Loader {
     constructor(config) {
         this.config = config;
         this.sprites = config.loader
-        this.sounds = config.soundLoader
+        //this.sounds = config.soundLoader
         this.resources = {};
     }
 
     async preload() {
         try {
                 await Promise.all(
-                    this.sprites.map(async (imageModule) => {
-                    let imagePath = imageModule.default;
-                    const texture = await Assets.load(imagePath);
-                    const indexOfSlash = imagePath.lastIndexOf('/')
-                    imagePath = imagePath.substr(indexOfSlash + 1)
-                    const indexOfDot = imagePath.lastIndexOf('.')
-                    imagePath = imagePath.substr(0, indexOfDot)
-                    this.resources[imagePath] = texture; // Store loaded textures
+                    this.sprites.map(async (fileModule) => {
+                    let filePath = fileModule.default;
+                    const texture = await Assets.load(filePath);
+                    const indexOfSlash = filePath.lastIndexOf('/')
+                    filePath = filePath.substr(indexOfSlash + 1)
+                    const indexOfDot = filePath.lastIndexOf('.')
+                    const extension = filePath.substr(indexOfDot + 1)
+                    const fileName = filePath.substr(0, indexOfDot)
+                    if (extension.toLowerCase() === 'mp3') {
+                        if (sound.exists(fileName)) {
+                            sound.remove(fileName)
+                        }
+                        sound.add(fileName, fileModule.default)
+                    }
+                    else {
+                        this.resources[fileName] = texture; // Store loaded textures
+                    }
+                    
                 })
               );
-
-              await Promise.all(
-                this.sounds.map(async (soundModule) => {
-                    let soundPath = soundModule.default;
-                    const indexOfSlash = soundPath.lastIndexOf('/')
-                    let soundName = soundPath.substr(indexOfSlash + 1)
-                    const indexOfDot = soundName.lastIndexOf('.')
-                    soundName = soundName.substr(0, indexOfDot)
-                    sound.add(soundName, soundModule.default)
-            })
-          );
         } catch (error) {
             console.error("Error loading assets:", error);
         }
